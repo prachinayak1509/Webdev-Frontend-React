4c9a68bfda8f6576178486e40bfc4d86e10995f7
prachinayak1509 <prachinayak1509@gmail.com>
2023-01-25 22:29:23 +0530

    footer component and css

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from "./DishDetailComponent";
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -23,6 +24,7 @@ class Main extends Component {
         <Menu dishes ={this.state.dishes}
         onClick={(dishId) => this.onDishSelect(dishId)}/>
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
+        <Footer/>
       </div>
     );
   }
