553ed9796488005d6241b124992df278a61654d1
prachinayak1509 <prachinayak1509@gmail.com>
2023-01-28 19:36:51 +0530

    single page application2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,58 +6,76 @@ import {
   CardText,
   CardBody,
   CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
 } from "reactstrap";
-import { render } from "react-dom";
+import { Link } from "react-router-dom";
+import { Render } from "react-dom";
 
- 
-  
-  function RenderDish(dish) {
-    if (dish != null) {
-      const comments = dish.comments.map((comment) => {
-        return (
-          <div className="col-12 col-md-5 m-1">            <div key={comment.id} className="col-12 col-md-5 m-1">
-              <h4>{comment.comment}</h4>
-              <p>
-                -- {comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-              </p>
-            </div>
-            </div>
-
-        );
-      });
-
-      
+function RenderDish(dish) {
+  if (dish != null) {
+    const comments = dish.comments.map((comment) => {
       return (
-        <div>
+        <div className="col-12 col-md-5 m-1">
           {" "}
-          <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-          <div>
-            <h2>Comments</h2>
-            <div className="row">{comments}</div>
+          <div key={comment.id} className="col-12 col-md-5 m-1">
+            <h4>{comment.comment}</h4>
+            <p>
+              -- {comment.author},
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comment.date)))}
+            </p>
           </div>
         </div>
       );
-    } else {
-      return <div></div>;
-    }
-  }
+    });
 
-  const DishDetail = (props) => {
     return (
-      <div className="container">
-        <div className="row">{RenderDish(props.dish)}</div>
+      <div>
+        {" "}
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+        <div>
+          <h2>Comments</h2>
+          <div className="row">{comments}</div>
+        </div>
       </div>
     );
+  } else {
+    return <div></div>;
   }
-    console.log("Dishdetail component render is invoked");
-    
-  
+}
+
+const DishDetail = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">{RenderDish(props.dish)}
+      {RenderDish(props.comment)}
+      </div>
+    </div>
+  );
+}
 
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -18,19 +18,35 @@ class Main extends Component {
       dishes: DISHES,
       comments: COMMENTS,
       promotions: PROMOTIONS,
-      leaders: LEADERS
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
       return (
-      <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-       />
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comments) => comments.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
       );
     }
+
     return (
       <div>
         <Header />
@@ -41,7 +57,8 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
-          <Route exact path="/contactus" component={Contact}/>
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
