c4b925ba2bfda98bfe668cd92a5a3a2c1403f059
prachinayak1509 <prachinayak1509@gmail.com>
2023-02-08 21:44:24 +0530

    react-redux-form validation 3

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -13,11 +13,10 @@ import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
 const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !val || val.length <= len;
-const minLength = (len) => (val) => !val && val.length >= len;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) =>
-  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
   constructor(props) {
@@ -148,7 +147,7 @@ class Contact extends Component {
                   />
                    <Errors
                     className="text-danger"
-                    model=".lasttname"
+                    model=".lastname"
                     show="touched"
                     messages={{
                       required: "Required",
@@ -200,6 +199,19 @@ class Contact extends Component {
                     name="email"
                     placeholder="Email"
                     className="form-control"
+                    validators={{
+                      required,
+                     validEmail
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".email"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                     validEmail:'Invalid emai. address'
+                    }}
                   />
                 </Col>
               </Row>
