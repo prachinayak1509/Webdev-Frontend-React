948e786b5ceef03ec767d3fb391c778feb2bd79b
prachinayak1509 <prachinayak1509@gmail.com>
2023-02-10 10:04:28 +0530

    commentForm 2

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -11,7 +11,7 @@ class CommentForm extends Component{
             isCommentFormModelOpen : false
         };
     }
-}
+
 
 render(){
     return(
@@ -25,13 +25,25 @@ render(){
         <ModalBody>
             <LocalForm>
                 <Row className="form-group">
-                    <Label>
-
+                    <Label htmlFor="rating" md={12}>
+Rating
                     </Label>
-                    <Col>
+                    <Col md={12}>
                     <Control.select
-
-                    />
+                      model=".rating"
+                      name="rating"
+                      id="rating"
+                      className="form-control"
+        
+                />
+                <option>
+                    Please Select
+                </option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
                     <Errors
                     
                     
@@ -39,12 +51,46 @@ render(){
                     </Col>
 
                 </Row>
+
+
+                <Row className="form-control">
+                    <Label htmlFor="author" md={12}>Your Name</Label>
+                    <Col md={12}>
+                        <Control.text
+                        model=".author"
+                        name="author"
+                        id="author"
+                        placeholder="First Name"
+                        className="form-control"
+                        />
+                    </Col>
+                </Row>
+
+                <Row className="form-control">
+                    <Label htmlFor="comment" md={12}>Comment</Label>
+                    <Col md={12}>
+                        <Control.textarea 
+                        model=".comment"
+                        id="comment"
+                        name="comment"
+                        className="form-control"
+                        rows="6"
+                        />
+
+                    </Col>
+                </Row>
+                <Row className="form-control">
+                    <Col>
+                    <Button type="submit" color="primary">Submit
+                    </Button>
+                    </Col>
+                </Row>
             </LocalForm>
         </ModalBody>
     </Modal>
 </React.Fragment>
     );
-
+    } 
 }
 
 export default CommentForm;
