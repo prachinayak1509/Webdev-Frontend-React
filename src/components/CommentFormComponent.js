47463f8b3677b2ab8ec629c11bd5d75b465842e3
prachinayak1509 <prachinayak1509@gmail.com>
2023-02-11 10:22:10 +0530

    commentForm 3

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -1,96 +1,119 @@
 import React, { Component } from "react";
-import {Button, Modal, ModalBody, ModalHeader, Label, Row, Col} from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Label,
+  Row,
+  Col,
+} from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
 
-class CommentForm extends Component{
-    constructor(props){
-        super(props);
+    this.state = {
+      isCommentFormModelOpen: false,
+    };
 
-        this.state={
-            isCommentFormModelOpen : false
-        };
-    }
+    this.handleCommentFormSubmit = this.handleCommentFormSubmit.bind(this);
+    this.toggleCommentFormModal = this.toggleCommentFormModal.bind(this);
+  }
 
+  handleCommentFormSubmit(values) {
+    console.log("current state is:" + JSON.stringify(values));
+    alert("current state is:" + JSON.stringify(values));
+  }
 
-render(){
-    return(
-<React.Fragment>
-    <Button>
-        <span className="fa fa-comments fa-lg"></span> Submit Comment
-    </Button>
+  toggleCommentFormModal() {
+    this.setState({
+      isCommentFormModalOpen: !this.state.isCommentFormModalOpen,
+    });
+  }
 
-    <Modal>
-        <ModalHeader>Submit Comment</ModalHeader>
-        <ModalBody>
-            <LocalForm>
-                <Row className="form-group">
-                    <Label htmlFor="rating" md={12}>
-Rating
-                    </Label>
-                    <Col md={12}>
-                    <Control.select
-                      model=".rating"
-                      name="rating"
-                      id="rating"
-                      className="form-control"
-        
-                />
-                <option>
-                    Please Select
-                </option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                    <Errors
-                    
-                    
-                    />
-                    </Col>
+  render() {
+    return (
+      <React.Fragment>
+        <Button outline onClick={this.toggleCommentFormModal}>
+          <span className="fa fa-comments fa-lg"></span> Submit Comment
+        </Button>
 
-                </Row>
+        <Modal
+          isOpen={this.state.isCommentFormModalOpen}
+          toggle={this.toggleCommentFormModal}
+        >
+          <ModalHeader toggle={this.toggleCommentFormModal}>
+            Submit Comment
+          </ModalHeader>
+          <ModalBody>
+            <LocalForm
+              onSubmit={(values) => this.handleCommentFormSubmit(values)}
+            >
+              <Row className="form-group">
+                <Label htmlFor="rating" md={12}>
+                  Rating
+                </Label>
+                <Col md={12}>
+                  <Control.select
+                    model=".rating"
+                    name="rating"
+                    id="rating"
+                    className="form-control"
+                  />
+                  <option>Please Select</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <Errors />
+                </Col>
+              </Row>
 
+              <Row className="form-control">
+                <Label htmlFor="author" md={12}>
+                  Your Name
+                </Label>
+                <Col md={12}>
+                  <Control.text
+                    model=".author"
+                    name="author"
+                    id="author"
+                    placeholder="First Name"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
 
-                <Row className="form-control">
-                    <Label htmlFor="author" md={12}>Your Name</Label>
-                    <Col md={12}>
-                        <Control.text
-                        model=".author"
-                        name="author"
-                        id="author"
-                        placeholder="First Name"
-                        className="form-control"
-                        />
-                    </Col>
-                </Row>
-
-                <Row className="form-control">
-                    <Label htmlFor="comment" md={12}>Comment</Label>
-                    <Col md={12}>
-                        <Control.textarea 
-                        model=".comment"
-                        id="comment"
-                        name="comment"
-                        className="form-control"
-                        rows="6"
-                        />
-
-                    </Col>
-                </Row>
-                <Row className="form-control">
-                    <Col>
-                    <Button type="submit" color="primary">Submit
-                    </Button>
-                    </Col>
-                </Row>
+              <Row className="form-control">
+                <Label htmlFor="comment" md={12}>
+                  Comment
+                </Label>
+                <Col md={12}>
+                  <Control.textarea
+                    model=".comment"
+                    id="comment"
+                    name="comment"
+                    className="form-control"
+                    rows="6"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-control">
+                <Col>
+                  <Button type="submit" color="primary">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
             </LocalForm>
-        </ModalBody>
-    </Modal>
-</React.Fragment>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>
     );
-    } 
+  }
 }
 
 export default CommentForm;
