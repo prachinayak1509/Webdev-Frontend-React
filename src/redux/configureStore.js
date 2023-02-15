df62896bf6e6f22f0c89a025ed3290fc500482cd
prachinayak1509 <prachinayak1509@gmail.com>
2023-02-15 22:58:02 +0530

    combining reducers 2

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,18 @@
-import{ createStore } from 'redux';
-import { Reducer,initialState } from './reducer';
+import{ createStore, commbineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-Reducer,
-initialState
+   commbineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders
+   })
     );
 
     return store;
